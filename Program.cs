c4af36ef0be6730a75ee54c105006e7eae8ab90c
Nickolai <datlovnick@mail.ru>
2022-07-04 21:52:15 +0300

    добавили блоксхему и ввели массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string[] array =  {"12", "23", "-1111", "dd", "=12)_"};
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+
+
+
+
