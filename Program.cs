b69c430de9efa42689d1d5a6dc1399778234f460
Nickolai <datlovnick@mail.ru>
2022-07-04 22:53:26 +0300

    добавил поиск длины результирующего массива

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
 ﻿
 
-string[] array =  {"12", "23", "-1111", "dd", "=12)_"};
-for (int i = 0; i < array.Length; i++)
+string[] array =  {"12", "23", "-1111", "dd", "=)_"};
+
+printArray(array);
+
+
+
+int GetLengthArray(string[] array)
 {
-    Console.WriteLine(array[i]);
+    int length = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string str;
+        str = array[i];
+        if (str.Length < 4)
+        {
+            length++;
+        }
+    }
+    return length;
 }
+
+void printArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+ int length = GetLengthArray(array);
+ Console.WriteLine(length);
 
 
 
